a7a9fdf3e897317f0bdb128ed8ca1e2a9bf02cf2
Andrey <hatfox@protonmail.com>
2017-08-03 14:50:40 +0300

    Yay! Now wrapped

--- lightstamp.js
+++ lightstamp.js
@@ -2,7 +2,10 @@
 Lightstamp v.1.0.0
 http://github.com/ctl/lightstamp
 */
-function updateTimestamps(){
+(function(root, factory) {
+  (typeof module === "object" && module.exports) ? module.exports = factory() : root.Lightstamp = factory();
+}(this, function() {
+    function updateTimestamps(){
 var els = document.getElementsByTagName("*"), i=els.length
 while (i--) {
   if (els[i].dataset['timestamp']){
@@ -10,3 +13,4 @@ while (i--) {
   }}}
 updateTimestamps();
 setInterval(function(){updateTimestamps()},1000)
+}));
--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 # Lightstamp
 Livestamp-like library for automatically updated timestamps (like "X minutes ago")
-Currently AMD and CommonJS not supported , sorry
 # Why?
 Livestamp.js and Timeago.js depends on jQuery + Moment. This library depends only on Moment.
 # Usage
