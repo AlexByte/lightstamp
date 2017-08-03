924d16b6849191782c8b6244a5193ad2567e0487
Andrey <hatfox@protonmail.com>
2017-08-03 15:20:01 +0300

    Formatted JS

--- lightstamp.js
+++ lightstamp.js
@@ -3,14 +3,19 @@ Lightstamp v.1.0.0
 http://github.com/ctl/lightstamp
 */
 (function(root, factory) {
-  (typeof module === "object" && module.exports) ? module.exports = factory() : root.Lightstamp = factory();
+    (typeof module === "object" && module.exports) ? module.exports = factory(): root.Lightstamp = factory();
 }(this, function() {
-    function updateTimestamps(){
-var els = document.getElementsByTagName("*"), i=els.length
-while (i--) {
-  if (els[i].dataset['timestamp']){
-    els[i].innerHTML = moment.unix(els[i].dataset['timestamp']).fromNow()
-  }}}
-updateTimestamps();
-setInterval(function(){updateTimestamps()},1000)
-}));
+    function updateTimestamps() {
+        var els = document.getElementsByTagName("*"),
+            i = els.length
+        while (i--) {
+            if (els[i].dataset['timestamp']) {
+                els[i].innerHTML = moment.unix(els[i].dataset['timestamp']).fromNow()
+            }
+        }
+    }
+    updateTimestamps();
+    setInterval(function() {
+        updateTimestamps()
+    }, 10000)
+}))
